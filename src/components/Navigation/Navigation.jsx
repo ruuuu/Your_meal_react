ad78aa489d18fc4fa5a1897091eaa9f36aca663c
ruuuu <rufinka_91@mail.ru>
2023-02-24 20:31:34 +0300

    add server

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,47 +1,44 @@
 import style from './Navigation.module.css'; // style - название объекта, название сами придумали. Нужен для покдлючения стилей
 import classNames from 'classnames';
 import { Container } from '../Container/Container';  // компонент Container
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { changeCategory } from '../../store/category/categorySlice.js';
+import { useEffect } from 'react';
+
 
 
 export const Navigation = () => {
 
-      const { category } = useSelector((state) => {  // useSelector это хук
+      const { category, activeCategory } = useSelector((state) => {  // useSelector это хук
 
+            return state.category;        // category = [ {}, {}, {}, {} ]
       })
 
+      const dispatch = useDispatch();   //  чтобы получить action. Вернет фукнцию
 
+      useEffect(() => { // useEffect это хук
+
+      }, [])
+
+
+
+
+      // useDispatch это хук
       return (
             <nav className={style.navigation}>
                   <Container className={style.container}>
                         <ul className={style.list}>
-                              <li className={style.item}>
-                                    <button className={classNames(style.button, style.button_burger, style.navigation__button_active)}>Бургеры</button>
-                              </li>
-                              <li className={style.item}>
-                                    <button className={classNames(style.button, style.button_snack)}>Закуски</button>
-                              </li>
-                              <li className={style.item}>
-                                    <button className={classNames(style.button, style.button_hotdog)}>Хот-доги</button>
-                              </li>
-                              <li className={style.item}>
-                                    <button className={classNames(style.button, style.button_combo)}>Комбо</button>
-                              </li>
-                              <li className={style.item}>
-                                    <button className={classNames(style.button, style.button_shawarma)}>Шаурма</button>
-                              </li>
-                              <li className={style.item}>
-                                    <button className={classNames(style.button, style.button_pizza)}>Пицца</button>
-                              </li>
-                              <li className={style.item}>
-                                    <button className={classNames(style.button, style.button_wok)}>Вок</button>
-                              </li>
-                              <li className={style.item}>
-                                    <button className={classNames(style.button, style.button_dessert)}>Десерты</button>
-                              </li>
-                              <li className={style.item}>
-                                    <button className={classNames(style.button, style.button_sauce)}>Соусы</button>
-                              </li>
+
+                              {category.map((item, i) => (
+                                    <li className={style.item}>
+                                          <button className={classNames(style.button, activeCategory === i ? style.button_active : '')} style={{ backgroundImage: `url(${item.image})` }}
+                                                onClick={() => {
+                                                      dispatch(changeCategory({ indexcategory: i })) // при клике, вызывается changeCategory() и возаращется action
+                                                }}> {item.rus} </button>
+                                    </li>
+                              )
+                              )}
+
                         </ul>
                   </Container>
             </nav>
--- src/store/category/categorySlice.js
+++ src/store/category/categorySlice.js
@@ -1,19 +1,18 @@
-// редьюсер
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 
-
-const initialState = {
+// редьюсер, на основе него создается action, с помощью которого можно управзлять данными внутри  текущего state:
+const initialState = {              // нач состяние
       category: [
-            { title: 'burger', rus: 'Бургеры', image: '/img/burger.png' },
-            { title: 'snack', rus: 'Закуски', image: '/img/snack.png' },
-            { title: 'hot-dog', rus: 'Хот-доги', image: '/img/hot-dog.png' },
-            { title: 'combo', rus: 'Комбо', image: '/img/combo.png' },
-            { title: 'shawarma', rus: 'Шаурма', image: '/img/shawarma.png' },
-            { title: 'pizza', rus: 'Пицца', image: '/img/pizza.png' },
-            { title: 'wok', rus: 'Вок', image: '/img/wok.png' },
-            { title: 'dessert', rus: 'Десерты', image: '/img/dessert.png' },
-            { title: 'sauce', rus: 'Соусы', image: '/img/sauce.png' },
+            // { title: 'burger', rus: 'Бургеры', image: '/img/burger.png' },  //    с сервера получем 
+            // { title: 'snack', rus: 'Закуски', image: '/img/snack.png' },
+            // { title: 'hot-dog', rus: 'Хот-доги', image: '/img/hot-dog.png' },
+            // { title: 'combo', rus: 'Комбо', image: '/img/combo.png' },
+            // { title: 'shawarma', rus: 'Шаурма', image: '/img/shawarma.png' },
+            // { title: 'pizza', rus: 'Пицца', image: '/img/pizza.png' },
+            // { title: 'wok', rus: 'Вок', image: '/img/wok.png' },
+            // { title: 'dessert', rus: 'Десерты', image: '/img/dessert.png' },
+            // { title: 'sauce', rus: 'Соусы', image: '/img/sauce.png' },
       ],
       error: '',
       activeCategory: 0,  // индекс активнйо категории
@@ -21,16 +20,50 @@ const initialState = {
 
 
 
+
+// createAsyncThunk встроенная фукния, выполняет асинхрон действия:
+export const categoryRequestAsync = createAsyncThunk(
+      'category/fetch',   // category/fetch название action, задали такое имя сами
+      (data, obj) => {
+
+            return fetch(`${API_URI}${POSTFIX}/category`)
+                  .then(req => req.json())  //  req-ответ от сервера, return req.json(). обрабатываем промис
+                  .catch(error => {
+                        return { error }    // возаращет объект, попадет в action.payload
+                  })
+      }  // результат этого колбэка попадает в action extraReducers:
+)
+
+
+
 // categorySlice - объект содержащий редьюсеры и actions
 const categorySlice = createSlice({
-      name: 'category',
+      name: 'category',                                           // сами придумали названеи
       initialState,
-      reducers: {
-
+      reducers: {                               // здесь будут actions:
+            changeCategory(state, action) {   //  Делает выбранную  категрию активной
+                  console.log('state ', state);
+                  console.log('action ', action);                                               // { type: category/changeCategory, payload: indexcategory: i }
+                  state.activeCategory = action.payload.indexcategory;                          // свойстов  indexcategory придумали сами(индекс активной катергрии)
+            },
+      },
+      // автмоатич создают action:
+      extraReducers: {
+            [categoryRequestAsync.pending.type]: (state) => {
+                  state.error = ''; // error это свойстов в initialState
+            },
+            [categoryRequestAsync.fulfilled.type]: (state, action) => {
+                  state.error = '';
+                  state.category = action.payload;  // сервер возаращет массив категрий. category это свовойство в initialState
+            },
+            [categoryRequestAsync.rejected.type]: (state, action) => {
+                  state.error = action.payload.error;
+            }
       }
 
 });
 
 
+export const { changeCategory } = categorySlice.actions;                // changeCategory функция при выозве которой возращается action(строка с данными: category/changeCategory)
 export default categorySlice.reducer;
 //export default categorySlice.action;
