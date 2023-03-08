5538f958b1ad9df1bc65b034d9148f0eb3cad966
ruuuu <rufinka_91@mail.ru>
2023-03-08 20:54:33 +0300

    change api url

--- src/const.js
+++ src/const.js
@@ -1,2 +1,2 @@
-export const API_URI = 'http://localhost:3024';
+export const API_URI = 'https://impossible-atlantic-cardamom.glitch.me'; //   http://localhost:3024
 export const POSTFIX = '/api/product';
